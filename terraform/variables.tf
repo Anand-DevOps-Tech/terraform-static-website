62eec4288a80fc202fd3ef594e1ca9fe0d9b8892
Anandhakumar <awsdevops1590@gmail.com>
2026-07-23 10:15:30 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,8 +22,15 @@ variable "environment" {
   default     = "dev"
 }
 
+variable "key_name" {
+  description = "EC2 Key Pair Name"
+  type        = string
+  default     = "project"
+}
+
 variable "git_repo_url" {
   description = "Git repository URL containing static website files (HTML, CSS, JS, Images)"
   type        = string
   default     = "https://github.com/Anand-DevOps-Tech/terraform-static-website.git"
 }
+
