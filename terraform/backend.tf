b36abc86ec272c1f6d9ef4301cb7c14bd23fc6ee
Anandhakumar <awsdevops1590@gmail.com>
2026-07-23 10:28:46 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-backendtf "
+    bucket = "terraform-bucket-backendtf"
     key    = "static-website/terraform.tfstate"
     region = "us-east-1"
   }
