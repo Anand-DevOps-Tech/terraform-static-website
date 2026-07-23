10f2a453f5a53633c83588b88bd7cef1ec64213e
Anandhakumar <awsdevops1590@gmail.com>
2026-07-23 10:14:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
